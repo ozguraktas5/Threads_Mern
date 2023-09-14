3aa86d3f244e1b316ebdbfb6c77cf60865c03b92
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-09-14 19:31:15 +0300

    14.09.2023

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import * as z from "zod";
 import { ChangeEvent, useState } from "react";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing} from '@/lib/uploadthing';
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [ files, setFiles ] = useState<File[]>([])
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -84,7 +88,21 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     }
 
-    // TODO: Update user profile
+    await updateUser({
+        name: values.name,
+        path: pathname,
+        username: values.username,
+        userId: user.id,
+        bio: values.bio,
+        image: values.profile_photo,
+       
+      });
+
+      if(pathname === '/profile/edit') {
+        router.back();
+      } else {
+        router.push('/');
+      }
   }
   return (
     <Form {...form}>
