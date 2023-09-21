56f2b09c63c04f6c3d3c1bff4c07a1f7ecc6b47d
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-09-21 19:55:34 +0300

    21.09.2023

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -74,13 +74,17 @@ const ThreadCard = ({
 
                 {isComment && comments.length > 0 && (
                     <Link href={`/thread/${id}`}>
-                        <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                        <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} repl{comments.length > 1 ? "ies" : "y"}</p>
+                       
+
                     </Link>
                 )}
             </div>
           </div>
         </div>
       </div>
+      
+
       
     </article>
   );
